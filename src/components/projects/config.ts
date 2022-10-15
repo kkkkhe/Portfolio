2756cb29ee1e18efe2ada42372bf4fcec18ab49f
Dennisss <denzeldenisq@gmail.com>
2022-10-15 16:46:27 +0300

    changes

--- src/components/projects/config.ts
+++ src/components/projects/config.ts
@@ -1,9 +1,8 @@
-// import pizza from '../../assets/pizza.png'
-import kinomore from '../../assets/kinomore.png'
-import portfolio from '../../assets/portfolio.png'
+import pizzaWebp from '../../shared/assets/pizza.webp'
+import kinomore from '../../shared/assets/kinomore.webp'
+import portfolio from '../../shared/assets/portfolio.webp'
 export const projects = [
-	// {title: 'PizzaStore-Clone', description: 'Pizza store clone with add to favorite feature', link: 'https://github.com/kkkkhe/PizzaStore-Clone', image: pizza},
+	{title: 'PizzaStore-Clone', description: 'Pizza store clone with add to favorite feature', link: 'https://github.com/kkkkhe/PizzaStore-Clone', image: pizzaWebp},
 	{title: 'movieSearch', description: 'KINOMORE clone Progressive Web Application with ofline feature, add to favorite and other', link: 'https://github.com/kkkkhe/movieSearch', image: kinomore},
-	{title: 'Portfolio', description: 'Software Developer Portfolio', link: 'https://github.com/kkkkhe/Portfolio', image: portfolio},
 	{title: 'Portfolio', description: 'Software Developer Portfolio', link: 'https://github.com/kkkkhe/Portfolio', image: portfolio},
 ]
