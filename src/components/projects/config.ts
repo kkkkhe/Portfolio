6ec3709d5676a63964b80a1cbd694979849b0c2e
Dennisss <denzeldenisq@gmail.com>
2022-10-15 16:37:51 +0300

    fix path

--- src/components/projects/config.ts
+++ src/components/projects/config.ts
@@ -1,6 +1,6 @@
 // import pizza from '../../assets/pizza.png'
 import kinomore from '../../assets/kinomore.png'
-import portfolio from '@/assets/portfolio.png'
+import portfolio from '../../assets/portfolio.png'
 export const projects = [
 	// {title: 'PizzaStore-Clone', description: 'Pizza store clone with add to favorite feature', link: 'https://github.com/kkkkhe/PizzaStore-Clone', image: pizza},
 	{title: 'movieSearch', description: 'KINOMORE clone Progressive Web Application with ofline feature, add to favorite and other', link: 'https://github.com/kkkkhe/movieSearch', image: kinomore},
